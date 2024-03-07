f8d7f38e2c22ba8bd76f5ec3b840914a64c1094b
Jan Besler <jan_besler@online.de>
2024-03-07 16:15:36 +0100

    transformer ready for training

---
+++ src/eFormer/Dataloader.py
@@ -0,0 +1,78 @@
+# %%
+# Standard Libraries
+import pandas as pd
+
+# Machine Learning
+import torch
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+
+# %% [markdown]
+# Training Data Set
+# recurrent forecast
+# 
+# 12 hours look back to predict next value
+
+# %%
+class TimeSeriesDataProcessor:
+    def __init__(self, dataframe, forecast, look_back, batch_size=64, train_size=0.7, test_size=0.5, random_state=42):
+        self.dataframe = dataframe
+        self.forecast = forecast
+        self.look_back = look_back
+        self.batch_size = batch_size
+        self.train_size = train_size
+        self.test_size = test_size
+        self.random_state = random_state
+
+    def shifted_data(self):
+        data = self.dataframe
+        forecast = self.forecast
+        look_back = self.look_back
+        shifts = range(forecast, look_back + forecast)
+        variables = data.columns
+        
+        shifted_columns = []
+        for column in variables:
+            for i in shifts:
+                shifted_df = data[[column]].shift(i)
+                shifted_df.rename(columns={column: f"{column} (lag {i})"}, inplace=True)
+                shifted_columns.append(shifted_df)
+        
+        data_shifted = pd.concat([data] + shifted_columns, axis=1)
+        data_shifted.dropna(inplace=True)
+        
+        return data_shifted
+
+    def prepare_datasets(self):
+        s_df = self.shifted_data().drop(['Wind speed (m/s)'], axis=1)
+        
+        # Splitting dataset
+        df_train, df_rem = train_test_split(s_df, train_size=self.train_size, random_state=self.random_state)
+        df_eval, df_test = train_test_split(df_rem, test_size=self.test_size, random_state=self.random_state)
+
+        # Wrapping datasets
+        self.train_dataset = TimeSeriesDataset(df_train)
+        self.test_dataset = TimeSeriesDataset(df_test)
+        self.eval_dataset = TimeSeriesDataset(df_eval)
+
+    def create_dataloaders(self):
+        self.prepare_datasets()
+
+        self.train_loader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        self.test_loader = DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False)
+        self.eval_loader = DataLoader(self.eval_dataset, batch_size=self.batch_size, shuffle=False)
+
+        return self.train_loader, self.test_loader, self.eval_loader
+
+class TimeSeriesDataset(Dataset):
+    def __init__(self, dataframe):
+        self.labels = dataframe.iloc[:, 0].values
+        self.features = dataframe.iloc[:, 1:].values
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        features = torch.tensor(self.features[idx], dtype=torch.float)
+        labels = torch.tensor(self.labels[idx], dtype=torch.float)
+        return features, labels
---
+++ src/eFormer/sparse_decoder.py
@@ -0,0 +1,118 @@
+# %% [markdown]
+# # Libraries
+# 
+
+# %%
+# standard
+import pandas as pd
+import numpy as np
+import math
+from math import sqrt
+
+# machine learning
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from eFormer.embeddings import PositionalEncoding
+from eFormer.sparse_attention import DetSparseAttentionModule
+
+# %% [markdown]
+# # Sparse Decoder
+
+# %%
+class SparseDecoder(nn.Module):
+    def __init__(self, d_model, n_heads, encoder_output_dim, forecast_horizon=22, max_len=5000, d_ff=None, dropout=0.1, activation="relu"):
+        super(SparseDecoder, self).__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.forecast_horizon = forecast_horizon
+        d_ff = d_ff or 4*d_model
+        self.dropout = nn.Dropout(dropout)
+        self.activation = F.relu if activation == "relu" else F.gelu
+
+        # Initialize PositionalEncoding
+        self.pos_encoder = PositionalEncoding(d_model, max_len)
+
+        # Sparse Attention Module for cross attention
+        self.cross_attention = DetSparseAttentionModule(d_model, n_heads, prob_sparse_factor=5)
+
+        # Feed-forward network components
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        
+        # Normalization layers
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+
+        # Output layer
+        self.output_layer = nn.Linear(d_model, 1)
+
+    def forward(self, encoder_output, attn_mask=None):
+        # Generate positional encodings
+        dummy_input = torch.zeros(self.forecast_horizon, self.d_model).unsqueeze(0)
+        pos_encodings = self.pos_encoder(dummy_input)
+
+        # Apply encoder-decoder attention using positional encodings as queries and encoder outputs as keys and values
+        attn_output = self.cross_attention(pos_encodings, encoder_output, encoder_output, attn_mask)
+        attn_output = self.norm1(attn_output + self.dropout(attn_output))
+
+        # Feed-forward network
+        ff_output = attn_output.transpose(-1, 1)  # Prepare for conv1d
+        ff_output = self.dropout(self.activation(self.conv1(ff_output)))
+        ff_output = self.dropout(self.conv2(ff_output))
+        ff_output = ff_output.transpose(-1, 1)  # Back to original dims
+        ff_output = self.norm2(attn_output + self.dropout(ff_output))
+
+        # Generate forecasts based on the attention output
+        forecasts = self.output_layer(ff_output).squeeze(-1)
+        
+        return forecasts
+
+# %%
+class DetSparseDecoder(nn.Module):
+    def __init__(self, d_model, n_heads, encoder_output_dim, forecast_horizon=1, max_len=5000, d_ff=None, dropout=0.1, activation="relu"):
+        super(DetSparseDecoder, self).__init__()
+        self.SparseDecoder = SparseDecoder(
+            encoder_output_dim=encoder_output_dim,
+            forecast_horizon=1,
+            d_model=d_model,
+            n_heads=n_heads
+        )
+
+    def forward(self, encoder_output):
+        # calculate attention
+        attention_output = self.SparseDecoder(encoder_output)
+
+        return attention_output
+
+# %%
+class ProbSparseDecoder(nn.Module):
+    def __init__(self, d_model, n_heads, encoder_output_dim, forecast_horizon=1, max_len=5000, d_ff=None, dropout=0.1, activation="relu"):
+        super(ProbSparseDecoder, self).__init__()
+        self.SparseDecoder_mean = SparseDecoder(
+            encoder_output_dim=encoder_output_dim,
+            forecast_horizon=1,
+            d_model=d_model,
+            n_heads=n_heads
+        )
+        self.SparseDecoder_var = SparseDecoder(
+            encoder_output_dim=encoder_output_dim,
+            forecast_horizon=1,
+            d_model=d_model,
+            n_heads=n_heads
+        )
+
+    def forward(self, encoder_output):
+        # split tensor to extract mean and variance
+        output_mean = encoder_output[0]
+        output_variance = encoder_output[1]
+        
+        # calculate attention
+        attention_output_mean = self.SparseDecoder_mean(output_mean)
+        attention_output_var = self.SparseDecoder_var(output_variance)
+
+        # Combine the processed means and variances
+        combined_output = torch.stack([attention_output_mean, attention_output_var], dim=0)
+
+        return combined_output
